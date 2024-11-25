ad30cfdec5b27005a621a49b9c8f0296f8c20946
whocares <flevn@protonmail.com>
2024-11-26 00:32:44 +0100

    rearranged elements
    also changed double to float and long long to int bc values are in the
    range [0, 100]

--- labs/memory_bound/data_packing/solution.h
+++ labs/memory_bound/data_packing/solution.h
@@ -1,6 +1,7 @@
 // NOTE: this lab is currently broken.
-// After migrating to a new compiler version the speedup is no longer measurable consistently.
-// You can still try to solve it to learn the concept, but the result is not guaranteed.
+// After migrating to a new compiler version the speedup is no longer measurable
+// consistently. You can still try to solve it to learn the concept, but the
+// result is not guaranteed.
 
 #include <array>
 
@@ -9,12 +10,11 @@ constexpr int N = 10000;
 constexpr int minRandom = 0;
 constexpr int maxRandom = 100;
 
-// FIXME: this data structure can be reduced in size
 struct S {
+  float d;
+  int l;
   int i;
-  long long l;
   short s;
-  double d;
   bool b;
 
   bool operator<(const S &s) const { return this->i < s.i; }
